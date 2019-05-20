2cc3559feb36c4d847a34eb6244cb56010e04044
Armando Fernandez <Armando Fernandez>
2019-05-19 20:30:34 -0700

    Chuck Norris Jokes Added

---
+++ MVC/ChuckNorrisApi/ChuckNorrisApi/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using ChuckNorrisApi.Models;
+
+namespace ChuckNorrisApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Chuck chuck = new Chuck();
+
+            return View(chuck);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Chuck chuck)
+        {
+            chuck.getJoke();
+            return View(chuck);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC/ChuckNorrisApi/ChuckNorrisApi/Models/Chuck.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ChuckNorrisApi.Models
+{
+    public class Chuck
+    {
+        string categoryEndPoint = "https://api.chucknorris.io/jokes/categories";
+        public List<string> categoryList = new List<string>();
+        public string defaultCategory = string.Empty;
+        public string chuckJoke = string.Empty;
+
+        public Chuck()
+        {
+            // Get Joke Categories.
+            dynamic categories = JsonConvert.DeserializeObject(getChuckData(categoryEndPoint));
+            
+            foreach (var category in categories)
+            {
+                categoryList.Add(category.Value.ToUpper().ToString());
+            }
+        }
+
+
+        public void getJoke()
+        {
+            const string RANDOM_ENDPOINT = "https://api.chucknorris.io/jokes/random";
+            const string CATEGORY_ENDPOINT = "https://api.chucknorris.io/jokes/random?category={0}";
+
+            if (defaultCategory != "Any Category")
+            {
+                dynamic joke = JsonConvert.DeserializeObject(getChuckData(String.Format(CATEGORY_ENDPOINT, defaultCategory)));
+                chuckJoke =  joke.value.ToString();
+            }
+            else
+            {
+                dynamic joke = JsonConvert.DeserializeObject(getChuckData(RANDOM_ENDPOINT));
+                chuckJoke = joke.value.Tostring();
+            }
+        }
+
+        public string getChuckData(string endPoint)
+        {
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create(endPoint);
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            
+            // Close.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            // Return data.
+            return responseFromServer;
+        }
+    }
+
+ 
+}
+
+
